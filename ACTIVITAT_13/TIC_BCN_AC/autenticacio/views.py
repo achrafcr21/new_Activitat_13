842a7344a5e1dca8f4a886bb91f5d72d0fbd5b38
Achraf Chakir <2023_achraf.chakir@iticbcn.cat>
2025-02-14 18:00:00 +0100

    vistes de login sense session

--- ACTIVITAT_13/TIC_BCN_AC/autenticacio/views.py
+++ ACTIVITAT_13/TIC_BCN_AC/autenticacio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import LoginForm
+from .models import Usuario
 
 # Create your views here.
 
@@ -7,7 +8,13 @@ def login_sin_sesion(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            return redirect('inicio')
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            try:
+                usuario = Usuario.objects.get(email=email, password=password)
+                return redirect('inicio')
+            except Usuario.DoesNotExist:
+                form.add_error(None, 'Credenciales erróneas')
     else:
         form = LoginForm()
     return render(request, 'autenticacio/login.html', {'form': form})
